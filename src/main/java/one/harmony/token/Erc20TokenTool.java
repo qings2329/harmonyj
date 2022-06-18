6021e8b3cbd823f4c43998f9f0a35949821130ec
li.min <li.min@bitget.com>
2022-06-18 14:23:31 +0800

    deploy token

--- src/main/java/one/harmony/token/Erc20TokenTool.java
+++ src/main/java/one/harmony/token/Erc20TokenTool.java
@@ -21,7 +21,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 
-public class Erc20Token extends Contract {
+public class Erc20TokenTool extends Contract {
 
     public static final String FUNC_NAME = "name";
 
@@ -35,7 +35,7 @@ public class Erc20Token extends Contract {
 
     public static final String FUNC_TRANSFER = "transfer";
 
-    protected Erc20Token(String contractBinary, String contractAddress, ContractGasProvider gasProvider, Handler handler) {
+    protected Erc20TokenTool(String contractBinary, String contractAddress, ContractGasProvider gasProvider, Handler handler) {
         super(contractBinary, contractAddress, handler, gasProvider);
     }
 
@@ -91,17 +91,40 @@ public class Erc20Token extends Contract {
 
 
     public static void main(String args[]) throws Exception {
-        String contractAddress = "one1ctppqspq688anm80uq29gyut04tpzjnfff04e2";
+//        String contractAddress = "one1tjn7gd6ur2qrs53f0euwvxqxc35c8cun4g0mzz";
+        String contractAddress = "0xfbd58c121d2d1a9190bbe15c66712b230f300bc1";
+//
+//        String contractAddress = "one1cjrqgc79n4v6ntavnl0rthlemgmrapp980fllp";
         String hexContractAddr = new one.harmony.account.Address(contractAddress, contractAddress.startsWith("0x")).getHexAddr();
 
-        String accountAddress = "one18t4yj4fuutj83uwqckkvxp9gfa0568uc48ggj7";
+//        String accountAddress = "one1pdv9lrdwl0rg5vglh4xtyrv3wjk3wsqket7zxy";
+        String accountAddress = "one1pdv9lrdwl0rg5vglh4xtyrv3wjk3wsqket7zxy";
         String hexAccountAddr = new one.harmony.account.Address(accountAddress, accountAddress.startsWith("0x")).getHexAddr();
 
         Handler handler = new Handler(Config.node, ChainID.TESTNET);
         StaticGasProvider gasProvider = new StaticGasProvider(new BigInteger("0"), new BigInteger("0"));
-        Erc20Token erc20Token = new Erc20Token("", hexContractAddr, gasProvider, handler);
+        Erc20TokenTool erc20Token = new Erc20TokenTool("", hexContractAddr, gasProvider, handler);
         BigInteger balance = erc20Token.balanceOf(hexAccountAddr).send();
-        System.out.println(balance);
+
+
+
+        try{
+            BigInteger decimal = erc20Token.decimals().send();
+            BigInteger ba = balance.divide(BigInteger.TEN.pow(decimal.intValue()));
+            System.out.println(ba);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println(balance);
+        }
+
+        try {
+            String symbol = erc20Token.symbol().send();
+            System.out.println(symbol);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("SYMBOL");
+        }
+
     }
 
 
--- src/test/java/one/harmony/cmd/KeysTest.java
+++ src/test/java/one/harmony/cmd/KeysTest.java
@@ -3,7 +3,12 @@ package one.harmony.cmd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import one.harmony.account.Address;
 import org.junit.jupiter.api.Test;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+
+import java.math.BigInteger;
 
 public class KeysTest {
 
@@ -57,5 +62,15 @@ public class KeysTest {
 	@Test
 	public void testCleanKeyStore() throws Exception {
 		Keys.cleanKeyStore();
+	}
+
+	@Test
+	public void testConvertFromPriv() throws Exception {
+		String priv = "01F903CE0C960FF3A9E68E80FF5FFC344358D80CE1C221C3F9711AF07F83A3BD";
+		ECKeyPair ecKeyPair = ECKeyPair.create(new BigInteger(priv, 16));
+		Credentials credentials = Credentials.create(ecKeyPair);
+		String address = credentials.getAddress();
+		String oneAddress = Address.toBech32(address);
+		System.out.println(oneAddress);
 	}
 }
